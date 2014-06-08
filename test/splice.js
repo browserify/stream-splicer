a81a597fd372d584545964dd494f0b508773dc4f
James Halliday <mail@substack.net>
2014-06-08 05:37:02 -0700

    failing splice test

---
+++ test/splice.js
@@ -0,0 +1,57 @@
+var pipeline = require('../');
+var through = require('through2');
+var split = require('split');
+var concat = require('concat-stream');
+var test = require('tape');
+
+test('splice', function (t) {
+    var expected = {};
+    expected.replacer = [ '333', '444', '5000' ];
+    
+    t.plan(Object.keys(expected).reduce(function (sum, key) {
+        return sum + expected[key].length;
+    }, 0) + 1);
+    
+    var a = split();
+    var b = through.obj(function (row, enc, next) {
+        this.push(JSON.parse(row));
+        next();
+    });
+    var c = through.obj(function (row, enc, next) {
+        this.push(row.x);
+        next();
+    });
+    var d = through.obj(function (x, enc, next) {
+        this.push(String(x * 111));
+        next();
+    });
+    var thousander = through.obj(function (x, enc, next) {
+        this.push(String(x * 1000));
+        next();
+    });
+    
+    var replacer = through(function (buf, enc, next) {
+        var ex = expected.replacer.shift();
+        t.equal(buf.toString(), ex);
+        this.push(buf.toString('hex') + '\n');
+        
+        if (expected.replacer.length === 2) {
+            stream.splice(3, 1, thousander);
+        }
+        next();
+    });
+    
+    var stream = pipeline([ a, b, c, d, replacer ]);
+    stream.pipe(concat(function (body) {
+        t.deepEqual(
+            body.toString(),
+            '333333\n343434\n35303030\n36303030\n'
+        );
+    }));
+    
+    stream.write('{"x":3}\n');
+    stream.write('{"x":4}\n');
+    stream.write('{"x":5}\n');
+    stream.write('{"x":6}');
+    stream.end();
+});
