77f417aeeb9583f5a9d40a73eb6fb80e7072d66e
James Halliday <mail@substack.net>
2014-06-09 13:49:22 -0700

    fixed the header example

--- example/header.js
+++ example/header.js
@@ -6,12 +6,9 @@ var split = require('split');
 var headerData = {};
 var headers = through.obj(function (buf, enc, next) {
     var line = buf.toString('utf8');
-    
-    if (line === '') { // break on \n\n
+    if (line === '') {
         this.push(headerData);
-        
-        // replace header parsing with json row parsing
-        outer.splice(0, 2, JSONStream.parse([ true ]));
+        outer.splice(1, 1, JSONStream.parse([ 'rows', true ]));
     }
     else {
         var m = /^(\S+):(.+)/.exec(line);
@@ -19,9 +16,7 @@ var headers = through.obj(function (buf, enc, next) {
         var value = m && m[2].trim();
         if (m) headerData[key] = value;
     }
-    
     next();
 });
-
 var outer = pipeline([ split(), headers, JSONStream.stringify() ]);
 process.stdin.pipe(outer).pipe(process.stdout);
