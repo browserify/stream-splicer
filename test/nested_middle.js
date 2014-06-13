cee0b80e3f8e576629c88ddfe3ee4d279a14ed30
James Halliday <mail@substack.net>
2014-06-12 17:56:49 -0700

    failing test for nested group after a stream

---
+++ test/nested_middle.js
@@ -0,0 +1,41 @@
+var pipeline = require('../');
+var through = require('through2');
+var stringify = require('JSONStream').stringify;
+var split = require('split');
+var concat = require('concat-stream');
+var test = require('tape');
+
+test('nested splicer', function (t) {
+    t.plan(1);
+    
+    var addNewLines = through(function (buf, enc, next) {
+        this.push(buf + '\n');
+        next();
+    });
+    
+    var stream = pipeline.obj([
+        through(function (buf, enc, next) {
+            this.push(String.fromCharCode(buf.charAt(0) + 5));
+            next();
+        }),
+        [ split(), addNewLines ],
+        through(function (buf, enc, next) {
+            this.push('> ' + buf);
+            next()
+        })
+    ]);
+    
+    stream.get(0).unshift(through(function (buf, enc, next) {
+        this.push(buf.toString('utf8').toUpperCase());
+        next();
+    }));
+    
+    stream.pipe(concat(function (body) {
+        t.deepEqual(body.toString(), '> F\n> G\n> H\n');
+    }));
+    
+    stream.write('a\n');
+    stream.write('b\n');
+    stream.write('c');
+    stream.end();
+});
