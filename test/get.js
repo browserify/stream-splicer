28bbcd75fc557e1d9535d43ffa089ede1b4bf07c
James Halliday <mail@substack.net>
2014-06-14 19:50:19 -0700

    nested get test

--- test/get.js
+++ test/get.js
@@ -22,3 +22,20 @@ test('get', function (t) {
     t.equal(pipe.get(-5), undefined, '-5');
     t.end();
 });
+
+test('nested get', function (t) {
+    var a = through.obj();
+    var b = through.obj();
+    var c = through.obj();
+    var d = through.obj();
+    var e = through.obj();
+    var f = through.obj();
+    var g = through.obj();
+    
+    var pipe = pipeline([ a, [ b, c, [ d, [ e ], f ] ], g ]);
+    t.equal(pipe.get(0), a);
+    t.equal(pipe.get(1, -1, 1, 0), e);
+    t.equal(pipe.get(1, 3), undefined);
+    t.equal(pipe.get(4, 3), undefined);
+    t.end();
+});
